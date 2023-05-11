f72d590784cef5444a9634824137fbdbceed7bf0
Abdul Rahman <64832120+Abdul-43@users.noreply.github.com>
2023-05-11 21:38:11 +0530

    Update ListUsers.jsx

--- src/components/ListUsers.jsx
+++ src/components/ListUsers.jsx
@@ -1,60 +1,80 @@
-import React ,{ useEffect, useState } from 'react';
-import { Container, Table, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Container, Table, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const ListUsers = () => {
-  // Fetch user data and display it here
   const [userList, setUserList] = useState([]);
 
   useEffect(() => {
-    // Fetch user data from API or data source
-    const fetchUserList = async () => {
-      try {
-        // Make an API request to fetch user list
-        const response = await fetch('/api/users');
-        const data = await response.json();
-        setUserList(data); // Set the fetched user list
-      } catch (error) {
-        console.error('Error fetching user list:', error);
-      }
-    };
-
     fetchUserList();
   }, []);
 
-  const handleEditUser = (userId) => {
-    // Handle edit user button click
-    console.log(`Edit user with ID: ${userId}`);
+  const fetchUserList = async () => {
+    try {
+      const response = await fetch(
+        "https://645cf892250a246ae313d573.mockapi.io/api/users/user"
+      );
+      const data = await response.json();
+      setUserList(data);
+    } catch (error) {
+      console.error("Error fetching user list:", error);
+    }
+  };
+
+  const handleDelete = async (user) => {
+    try {
+      const response = await fetch(
+        `https://645cf892250a246ae313d573.mockapi.io/api/users/user/${user.id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        alert(`${user.name} User deleted successfully`);
+        fetchUserList();
+      } else {
+        console.error("Error deleting user");
+      }
+    } catch (error) {
+      console.error("Error deleting user:", error);
+    }
   };
 
   return (
-    <Container className="table-container mt-0 ">
-      <br/>
-      <br/>
+    <Container className="table-container">
       <h1>List Users</h1>
-      <Table striped bordered hover className='mt-5' style={{border:"2px solid"}}>
+      <br />
+      <Table striped bordered hover style={{ border: "2px solid" }}>
         <thead>
           <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
-            <th>Action</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {/* Render user list */}
           {userList.map((user) => (
             <tr key={user.id}>
               <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
-              <td>
-              <Button variant="primary" onClick={() => handleEditUser(1)}>
-                Edit
-              </Button>
-            </td>
-         </tr>
-         ))}
-          {/* Add more table rows */}
+              <td className="d-flex justify-content-around">
+                <Link
+                  to={`/edit-user/${user.id}`}
+                  className="btn btn-primary mr-2"
+                >
+                  Edit
+                </Link>
+                <Link to={`/profile/${user.id}`} className="btn btn-info mr-2">
+                  Profile
+                </Link>
+                <Button variant="danger" onClick={() => handleDelete(user)}>
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </Container>
